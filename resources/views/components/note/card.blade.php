59e9f9e340020751771db57d05dfcfd7094ce74e
xynz <sqdcontrast@gmail.com>
2025-01-13 17:43:19 +0300

    feat(ui): add style for note.card component

--- resources/views/components/note/card.blade.php
+++ resources/views/components/note/card.blade.php
@@ -1,4 +1,7 @@
-<div>
-    <h2>{{ $title }}</h2>
-    <p>{{ $content }}</p>
+<div class="border py-5 px-4">
+    <div {{ $attributes }}>
+        <h2 class="text-3xl font-medium mb-2">{{ $title }}</h2>
+        <p class="text-2xl font-normal">{{ $content }}</p>
+    </div>
+    {{ $slot }}
 </div>
