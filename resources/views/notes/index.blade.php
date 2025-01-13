fe223997364d2c29a7f702b5bfb6cb7412e1b819
xynz <sqdcontrast@gmail.com>
2025-01-13 17:45:43 +0300

    feat(ui): add styles for notes.index view and add card buttons

--- resources/views/notes/index.blade.php
+++ resources/views/notes/index.blade.php
@@ -1,19 +1,21 @@
 @extends('layouts.base')
 
 @section('main')
-    <ul>
-        @foreach ($notes as $note)
-            <li>
-                <div>
-                    <x-note.card :title="$note->title" :content="$note->content" />
-                    <div>
-                        <x-form.form method="POST" action="{{route('notes.delete', $note->id)}}">
-                            @method('DELETE')
-                            <button>delete</button>
-                        </x-form.form>
-                    </div>
-                </div>
-            </li>
-        @endforeach
-    </ul>
+    <div class="container mx-auto px-4">
+        <ul>
+            @foreach ($notes as $note)
+                <li>
+                    <x-note.card :title="$note->title" :content="$note->content" class="mb-2">
+                        <div>
+                            <x-form.form method="POST" action="{{ route('notes.delete', $note->id) }}">
+                                @method('DELETE')
+                                <x-note.card-button>delete</x-note.card-button>
+                                <x-note.card-button href="{{route('notes.edit', $note->id)}}">edit</x-note.card-button>
+                            </x-form.form>
+                        </div>
+                    </x-note.card>
+                </li>
+            @endforeach
+        </ul>
+    </div>
 @endsection
