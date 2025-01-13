c8b0cb98b508d1ba2bae3aaa43dce6372c94f694
xynz <sqdcontrast@gmail.com>
2025-01-13 16:43:27 +0300

    feat(ui): add and style footer markup

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,1 +1,9 @@
-<footer></footer>
+<footer class="py-8">
+    <div class="container mx-auto px-4">
+        <p class="text-center">
+            <span>&copy;</span>
+            made by xynz
+            <span>&#9829;</span>
+        </p>
+    </div>
+</footer>
