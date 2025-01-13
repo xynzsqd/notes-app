2b4bc56ab28af93e6d494ef149ddc2b733a9ec45
xynz <sqdcontrast@gmail.com>
2025-01-13 16:49:26 +0300

    feat(ui): add style footer text

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="py-8">
     <div class="container mx-auto px-4">
-        <p class="text-center">
+        <p class="text-center text-xl font-medium">
             <span>&copy;</span>
             made by xynz
             <span>&#9829;</span>
