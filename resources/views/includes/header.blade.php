68043f8ecaf3d4a3d5a01f42d5b7b784c888ca2f
xynz <sqdcontrast@gmail.com>
2025-01-13 16:38:46 +0300

    feat(ui): add and style header markup

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,1 +1,12 @@
-<header></header>
+<header class="py-8">
+    <div class="container mx-auto px-4">
+        <nav class="flex justify-between items-center text-2xl font-black">
+            <a href="{{ route('notes.index') }}" class="flex">
+                <h1>MyNotes</h1>
+            </a>
+            <a href="{{route('notes.create')}}" class="flex bg-indigo-600 text-white px-3 py-2 rounded-md">
+                Create note
+            </a>
+        </nav>
+    </div>
+</header>
