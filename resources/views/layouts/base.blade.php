707dd0c1ba0156477b19e4360ade3f78dfd4bd9a
xynz <sqdcontrast@gmail.com>
2025-01-13 16:29:37 +0300

    chiore: add Tailwind CSS via CDN

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -1,16 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('page.title', config('app.name'))</title>
 </head>
+
 <body>
     @include('includes.header')
     <main>
         @yield('main')
     </main>
     @include('includes.footer')
+    <script src="https://cdn.tailwindcss.com"></script>
 </body>
+
 </html>
