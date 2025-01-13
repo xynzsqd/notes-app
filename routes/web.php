7ea40b843a4d4be9879a86c585ec50a98805797d
xynz <sqdcontrast@gmail.com>
2025-01-13 10:20:31 +0300

    feat(routes, controller): create routes and controller for notes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\NoteController;
 
-Route::get('/', function () {
-    return view('notes.index');
-});
+Route::get('/notes', [NoteController::class, 'index'])->name('notes.index');
+Route::get('/notes/create', [NoteController::class, 'create'])->name('notes.create');
+Route::post('/notes', [NoteController::class, 'store'])->name('notes.store');
+Route::post('/notes/{note}', [NoteController::class, 'show'])->name('notes.show');
+Route::get('/notes/{note}/edit', [NoteController::class, 'edit'])->name('notes.edit');
+Route::put('/notes/{note}', [NoteController::class, 'update'])->name('notes.update');
+Route::delete('/notes/{note}', [NoteController::class, 'delete'])->name('notes.delete');
