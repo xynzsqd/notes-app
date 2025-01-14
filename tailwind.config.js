49c2289c031d23df9165285de2140168f9845dc5
xynz <sqdcontrast@gmail.com>
2025-01-14 17:25:37 +0300

    chore(config): enable custom colors in tailwind.config.js by uncommenting line

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,8 @@ export default {
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                // temporary commented for tailwind intellisense
+                // sans: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
         },
     },
